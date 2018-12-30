36e3350c7da9c36d130d085cec2a1ef1c33b930c
max1mka1 <maxinstellar@gmail.com>
2018-12-31 01:20:02 +0500

    node_modules should be ignored!

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6bf1e1ece8795897b619f1c0c4abc6b7.js"
+  "/my_resume2/precache-manifest.7b1ec1b653e9a711b28840ef073777bc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/my_resume2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
