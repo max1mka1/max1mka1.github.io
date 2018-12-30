b3df51ceee5132d935865a98777fbe00b4dd239e
max1mka1 <maxinstellar@gmail.com>
2018-12-31 03:48:26 +0500

    Commit photo changes + URL

--- build/precache-manifest.fd6b4583202e9a65b516b5305308a60f.js
+++ build/precache-manifest.fd6b4583202e9a65b516b5305308a60f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/my_resume2/static/js/runtime~main.e88605d6.js"
   },
   {
-    "revision": "72b45d8dcf8d2dcdcb33",
-    "url": "/my_resume2/static/js/main.72b45d8d.chunk.js"
+    "revision": "743626e99aab0e4946d5",
+    "url": "/my_resume2/static/js/main.743626e9.chunk.js"
   },
   {
     "revision": "d21d843fc039373b1e05",
     "url": "/my_resume2/static/js/1.d21d843f.chunk.js"
   },
   {
-    "revision": "72b45d8dcf8d2dcdcb33",
+    "revision": "743626e99aab0e4946d5",
     "url": "/my_resume2/static/css/main.a2cbd961.chunk.css"
   },
   {
-    "revision": "12f9b1187cd4ebe81b1451305a9cd4e6",
+    "revision": "dfb5c8d049aa127f25f4af65ec0d8594",
     "url": "/my_resume2/index.html"
   }
 ];
