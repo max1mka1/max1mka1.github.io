88d2dd8c5978b4e1e8725ea1bb4bfbd529f857d6
max1mka1 <maxinstellar@gmail.com>
2018-12-31 16:11:00 +0500

    Изменена ссылка на #resume

--- build/precache-manifest.d19b1e94e34438eddc94b7d4c4c42b91.js
+++ build/precache-manifest.d19b1e94e34438eddc94b7d4c4c42b91.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/my_resume2/static/js/runtime~main.e88605d6.js"
   },
   {
-    "revision": "24d6bc927855b94d31f0",
-    "url": "/my_resume2/static/js/main.24d6bc92.chunk.js"
+    "revision": "c5ca1609d69b53022299",
+    "url": "/my_resume2/static/js/main.c5ca1609.chunk.js"
   },
   {
     "revision": "d21d843fc039373b1e05",
     "url": "/my_resume2/static/js/1.d21d843f.chunk.js"
   },
   {
-    "revision": "24d6bc927855b94d31f0",
+    "revision": "c5ca1609d69b53022299",
     "url": "/my_resume2/static/css/main.a2cbd961.chunk.css"
   },
   {
-    "revision": "c255ed3d95fe43e546a0ff0461933fbd",
+    "revision": "24fd1b0b3efb7cfec5d8fc47e1329e3f",
     "url": "/my_resume2/index.html"
   }
 ];
