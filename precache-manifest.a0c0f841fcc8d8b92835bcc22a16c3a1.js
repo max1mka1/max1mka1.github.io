160c7a5715ef82664fb909ca34f0df38ee88284e
max1mka1 <maxinstellar@gmail.com>
2018-12-31 16:03:22 +0500

    Updates

--- precache-manifest.a0c0f841fcc8d8b92835bcc22a16c3a1.js
+++ precache-manifest.a0c0f841fcc8d8b92835bcc22a16c3a1.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/my_resume2/static/js/runtime~main.e88605d6.js"
   },
   {
-    "revision": "4f2593f2947dbdb3dec2",
-    "url": "/my_resume2/static/js/main.4f2593f2.chunk.js"
+    "revision": "24d6bc927855b94d31f0",
+    "url": "/my_resume2/static/js/main.24d6bc92.chunk.js"
   },
   {
     "revision": "d21d843fc039373b1e05",
     "url": "/my_resume2/static/js/1.d21d843f.chunk.js"
   },
   {
-    "revision": "4f2593f2947dbdb3dec2",
+    "revision": "24d6bc927855b94d31f0",
     "url": "/my_resume2/static/css/main.a2cbd961.chunk.css"
   },
   {
-    "revision": "1fe0c8db542e91f7184d8dc7663783b9",
+    "revision": "c255ed3d95fe43e546a0ff0461933fbd",
     "url": "/my_resume2/index.html"
   }
 ];
